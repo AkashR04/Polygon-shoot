fbb3a2c26db4feae62a5141437b2ac6a77fa0a3d
AkashR04 <akashr424@protonmail.com>
2026-04-23 07:38:55 +0530

    Added player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,18 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 #const JUMP_VELOCITY = -400.0
-
+func get_input():
+	# This makes the characters right side keep facing the position the mouse is in.
+	look_at(get_global_mouse_position())
+	#Now lets define velocity to set CharacterBody2D's velocity
+	#Lets get Vector2D local nodes's x axis by using transform.x
+	#lets get -1 or 1 according to key binding which is passed on to input.getAxis([-1],[1])
+	#Multiply it by variable speed to define the speed at which it would be moving example -300 or 300
+	velocity.x = Input.get_axis("left","right") * SPEED
+	velocity.y = Input.get_axis("up","down") * SPEED
+	#Lets use lerp function to make the movement a little bit smoother
+	#This makes adding real velocity to current velocity bit smoother
+	velocity = lerp(get_real_velocity(), velocity, 0.1)
 
 func _physics_process(delta: float) -> void:
 	# To Add the gravity. but we don't need it so lets comment it
@@ -16,10 +27,12 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	#var direction := Input.get_axis("ui_left", "ui_right")
+	#if direction:
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+#
+	# Calling function to move according to input, scratch that idea since everytime it calls the function it will just get the top input
+	get_input()
 	move_and_slide()
